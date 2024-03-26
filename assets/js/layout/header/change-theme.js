3b7220b35f3de9c92a8202d2b3c0cb14a663bf56
Ramferi <ramferi@python.dev>
2024-03-25 20:00:24 -0600

    :zap:

--- assets/js/layout/header/change-theme.js
+++ assets/js/layout/header/change-theme.js
@@ -1,20 +1,23 @@
 'use strict';
 // Importaciones
-import { getSystemTheme } from "../../utilities/dom.js";
+import { getSystemTheme, changeImgSrc } from "../../utilities/dom.js";
 
 // Variables y constantes
 const changeThemeBtn = document.getElementById('changeThemeBtn');
 const themeSelected = localStorage.getItem('theme') || getSystemTheme();
+const themeIcon = document.querySelector('#changeThemeBtn img');
 
 // Funciones
 const enableDarkTheme = () => {
     document.body.classList.add('dark');
     localStorage.setItem('theme', 'dark');
+    changeImgSrc(themeIcon, 'assets/icons/sun.png');
 }
 
 const enableLightTheme = () => {
     document.body.classList.remove('dark');
     localStorage.setItem('theme', 'light');
+    changeImgSrc(themeIcon, 'assets/icons//moon.png');
 }
 
 // Event Listeners
